6e122f0ff44a96c9bf17b0ebe4f7d78d412497b6
Maryam <maryam.mehrabian@bouvet.no>
2026-06-30 14:42:51 +0200

    Added architecture.html

--- script.js
+++ script.js
@@ -1,14 +1,22 @@
 const originalTitle = document.title;
 const favicon = document.getElementById("favicon");
 
-document.querySelectorAll("nav a").forEach(link => {
-    link.addEventListener("click", e => {
+const links = document.querySelectorAll("nav a");
+
+links.forEach(link => {
+    link.addEventListener("click", function (e) {
+        if (this.hostname !== window.location.hostname) return;
+
         e.preventDefault();
 
-        document.body.classList.add("fade-out");
+        document.body.classList.add("page-exit");
 
         setTimeout(() => {
-            window.location = link.href;
-        }, 500);
+            window.location.href = this.href;
+        }, 300);
     });
+});
+
+window.addEventListener("pageshow", () => {
+    document.body.classList.remove("page-exit");
 });
